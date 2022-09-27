756abb74d557ce658402a5eaafc52a7fdec10bfa
sim wook <astralkaiser89@gmail.com>
2022-09-27 18:10:54 +0900

    Route state study

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,11 +1,49 @@
-import {useParams} from "react-router-dom";
+import {useLocation, useParams} from "react-router-dom";
+import styled from "styled-components";
+import {useState} from "react";
+
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 interface RouteParams {
     coinId: string;
 }
+
+interface RouteState {
+    state: string;
+}
+
 function Coin() {
+    const [loading, setLoading] = useState(true);
     const { coinId } = useParams<keyof RouteParams>();
-    return <h1>Coin: {coinId}</h1>;
+    const { state } = useLocation() as RouteState;
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.toString() || "loading..." }</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : null}
+        </Container>
+    );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,29 +1,95 @@
 import styled from "styled-components";
+import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Container = styled.div`
+  padding: 0px 20px;
 `;
 
 const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CoinsList = styled.ul`
+  
 `;
 
 const Coin = styled.li`
+  background-color: white;
+  color: ${props => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${props => props.theme.accentColor}
+    }
+  }
 `;
 
 const Title = styled.h1`
-  color: ${props => props.theme.accentColor}
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
 `;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string
+}
+
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        (async () => {
+            const response = await axios("https://api.coinpaprika.com/v1/coins");
+            setCoins(response.data.slice(0,100));
+            setLoading(false);
+        })();
+    }, []);
     return (
         <Container>
             <Header>
                 <Title>Coin</Title>
             </Header>
-            <CoinsList>
-                <Coin></Coin>
+            {loading ? (
+                <Loader>Loading...</Loader>
+                ) : (<CoinsList>
+                {coins.map(coin => (
+                    <Coin key={coin.id}>
+                        <Link to={`/${coin.id}`} state={coin.name}>
+                            <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}/>
+                            {coin.name} &rarr;
+                        </Link>
+                    </Coin>
+                ))}
             </CoinsList>
+                )}
         </Container>
     );
 }
