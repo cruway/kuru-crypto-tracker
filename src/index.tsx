d36d58d746f35c5caa3f7eec16381b25a146dc91
sim wook <astralkaiser89@gmail.com>
2022-09-27 11:52:25 +0900

    setup ready

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {ThemeProvider} from "styled-components";
-import {darkTheme, lightTheme} from "./theme";
+import {theme} from "./theme";
 
 
 // typescript形式に変更する場合、as HTMLElement追加
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <ThemeProvider theme={darkTheme}>
-          <App />
-      </ThemeProvider>
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
 );
