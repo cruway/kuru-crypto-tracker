336af98b28d947eea2973ebf15df8ebe3e479b0c
sim wook <astralkaiser89@gmail.com>
2022-09-27 13:54:50 +0900

    Coins init

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,10 +1,31 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+`;
+
+const Header = styled.header`
+`;
+
+const CoinsList = styled.ul`
+`;
+
+const Coin = styled.li`
+`;
+
 const Title = styled.h1`
   color: ${props => props.theme.accentColor}
 `;
 function Coins() {
-    return <Title>Coins</Title>;
+    return (
+        <Container>
+            <Header>
+                <Title>Coin</Title>
+            </Header>
+            <CoinsList>
+                <Coin></Coin>
+            </CoinsList>
+        </Container>
+    );
 }
 
 export default Coins;
