b4f269857c0b624283db6e6b782fb278ce839d1a
sim wook <astralkaiser89@gmail.com>
2022-09-28 00:40:55 +0900

    Route state study

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,9 @@
-import {useLocation, useParams} from "react-router-dom";
+import {Outlet, Route, Routes, useLocation, useMatch, useParams} from "react-router-dom";
 import styled from "styled-components";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Chart from "./Chart";
+import Price from "./Price";
 
 
 const Container = styled.div`
@@ -14,6 +17,31 @@ const Header = styled.header`
   align-items: center;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 4px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 const Title = styled.h1`
   font-size: 48px;
   color: ${props => props.theme.accentColor};
@@ -24,6 +52,17 @@ const Loader = styled.span`
   display: block;
 `;
 
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  
+`;
+
 interface RouteParams {
     coinId: string;
 }
@@ -32,16 +71,116 @@ interface RouteState {
     state: string;
 }
 
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    logo: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface PriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: {
+            ath_data: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        }
+    };
+}
+
 function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams<keyof RouteParams>();
     const { state } = useLocation() as RouteState;
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
+    const priceMatch = useMatch("/:coinId/price");
+    const chartMatch = useMatch("/:coinId/chart");
+    useEffect(() => {
+        (async () => {
+            const infoData = await axios(`https://api.coinpaprika.com/v1/coins/${coinId}`);
+            const priceData = await axios(`https://api.coinpaprika.com/v1/tickers/${coinId}`);
+            setInfo(infoData.data);
+            setPriceInfo(priceData.data);
+            setLoading(false);
+        })();
+    }, [coinId]);
     return (
         <Container>
             <Header>
-                <Title>{state?.toString() || "loading..." }</Title>
+                <Title>{state?.toString() ? state : loading ? "Loading..." : info?.name }</Title>
             </Header>
-            {loading ? <Loader>Loading...</Loader> : null}
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>{info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{info?.open_source ? "Yes" : "No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Suply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>MAX Suply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Outlet />
+                </>
+            )}
         </Container>
     );
 }
