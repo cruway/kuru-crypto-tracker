647b4a4c81763e1d7095267ce3149e5b370133e7
sim wook <astralkaiser89@gmail.com>
2022-09-27 13:47:27 +0900

    style setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import Router from "./Router";
+import {createGlobalStyle} from "styled-components";
+import reset from "styled-reset";
 
+const GlobalStyle = createGlobalStyle`
+  ${reset} // reset css
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Source+Sans+Pro&display=swap'); // font install
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: ${props => props.theme.bgColor};
+    color: ${props => props.theme.textColor}
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 function App() {
-    return <Router/>;
+    return (
+        <>
+            <GlobalStyle/>
+            <Router/>
+        </>
+    );
 }
 
 export default App;
